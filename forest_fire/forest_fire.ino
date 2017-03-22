0764a4a7d7037ab991d4afeea727374bba7ac7f0
rpshah96 <rpshah96@gmail.com>
2017-03-22 12:37:38 +0530

    Fire Detection by measuring CO2,Temperature,Humidity

--- forest_fire/forest_fire.ino
+++ forest_fire/forest_fire.ino
@@ -1,54 +1,51 @@
+#include "dht.h"
 
-#include <dht.h>
+#define DHTPIN 2     // what digital pin we're connected to
 
-dht DHT;
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
-#define DHT22_PIN 5
+DHT dht(DHTPIN, DHTTYPE);
 
-int gasIn = A0;
+int CO2_PIN = 5;
 int alarm = 13;
 
-int normalGas = 550;
-int normalTemp = 35;
-int normalHum = 20;
+int maxCO2 = 5000;
+float maxTemp = 50;
+float minHum = 12;
+
+float hum = 0;
+float temp = 0;
+
+int value_CO2 = 0;
+int count_CO2 = 0;
+
+const boolean DEBUG = true;
 
 void setup()
 {
-    pinMode(gasIn,INPUT);
+    pinMode(CO2_PIN,INPUT);
     pinMode(alarm,OUTPUT);
     Serial.begin(9600);
-    Serial.println("dht22_test.ino");
-    Serial.print("LIBRARY VERSION: ");
-    Serial.println(DHT_LIB_VERSION);
-    Serial.println();
-    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
+    
+    dht.begin();
+    
+    if(DEBUG)
+    {
+      Serial.println("Program started");
+      Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");  
+    }
 }
 
 void loop()
 {
-    int gasVal;
-    gasVal = analogRead(gasIn);
-    Serial.print("CO2 : \t");
-    Serial.print(gasVal,DEC);
-    Serial.println("");
-    // READ DATA
-    Serial.print("DHT22, \t");
-
-    uint32_t start = micros();
-    int chk = DHT.read22(DHT22_PIN);
-    uint32_t stop = micros();
-
-    // DISPLAY DATA
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t");
-    Serial.print(DHT.temperature, 1);
-    Serial.print(",\t");
-    Serial.print(stop - start);
-    Serial.println();
-
-
-    if( (DHT.humidity <= normalHum && DHT.humidity != 0.0 ) || ( DHT.temperature >= normalTemp && DHT.temperature != 0.0) || gasVal >= normalGas){
-      Serial.println("Fire! Fire! Fire!");
+    int co2 = CO2_sensor();
+    DHT_sensor();
+    if(( co2 >= maxCO2 ) && ( temp >= maxTemp || hum <=minHum ) ) 
+    {
+      if(DEBUG)
+      {
+        Serial.println("Fire! Fire! Fire!");  
+      }
       digitalWrite(alarm,HIGH);  
     }
     else{
@@ -58,6 +55,76 @@ void loop()
     
     delay(2000);
 }
-//
-// END OF FILE
-//
+
+
+int CO2_sensor()
+{
+  // Serial.println("get Co2");
+  unsigned long Th = pulseIn(CO2_PIN, HIGH, 1004000);
+
+  Th = Th / 1000;
+
+  if (Th > 2)
+  {
+    unsigned long value = ((2000 * (Th - 2)) / (Th + (1004 - Th) - 4));
+    value_CO2 += value;
+    count_CO2 += 1;
+  }
+
+  if (DEBUG)
+  {
+    Serial.print("CO2 : ");
+    Serial.print(value_CO2 / count_CO2);
+    Serial.println("ppm     ");
+  }
+
+  return value_CO2 / count_CO2;
+}
+
+void DHT_sensor()
+{
+  // Serial.println("get Temperature,Humidity");
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if ( ( isnan(h) || isnan(t) || isnan(f) ) && DEBUG ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  else{
+    hum = h;
+    temp = t;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+    if(DEBUG)
+    {
+      // DISPLAY DATA
+
+      Serial.print("Humidity: ");
+      Serial.print(h);
+      Serial.print(" %\t");
+      Serial.print("Temperature: ");
+      Serial.print(t);
+      Serial.print(" *C ");
+      Serial.print(f);
+      Serial.print(" *F\t");
+      Serial.print("Heat index: ");
+      Serial.print(hic);
+      Serial.print(" *C ");
+      Serial.print(hif);
+      Serial.println(" *F");
+
+    }
+}
+
+
