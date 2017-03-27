c08a0ecc1f242a233708d341e7d48f7abda0f27e
rpshah96 <rpshah96@gmail.com>
2017-03-27 08:55:00 +0530

    Smart Dustbin connected to Blynk App

--- smart_dustbin/smart_dustbin.ino
+++ smart_dustbin/smart_dustbin.ino
@@ -1,3 +1,15 @@
+//#define BLYNK_USE_DIRECT_CONNECT
+
+#define BLYNK_PRINT Serial
+
+#include <BlynkSimpleCurieBLE.h>
+#include <CurieBLE.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "7a5c09da42f14be8b012c8aea941d7d6";
+
+BLEPeripheral  blePeripheral;
 
 // defines pins numbers
 const int trigPin = 3;
@@ -25,10 +37,25 @@ void setup() {
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
   pinMode(LED5, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
+
+  Serial.begin(9600);
+  delay(1000);
+  blePeripheral.setLocalName("Blynk");
+  blePeripheral.setDeviceName("Blynk");
+  blePeripheral.setAppearance(384);
+  Serial.println("ble will start now start");
+  Blynk.begin(auth, blePeripheral);
+  blePeripheral.begin();
+  
+
+  Serial.println("Waiting for connections...");
+
 }
 
 void loop() {
+  Blynk.run();
+  blePeripheral.poll();
+
   int d = findDistance();
   showStatus(d);
   delay(100);
@@ -66,20 +93,23 @@ void showStatus(int dist){
     digitalWrite(LED3,LOW);
     digitalWrite(LED2,LOW);
     digitalWrite(LED1,LOW);
+    Blynk.virtualWrite(V1, 0);
   }
   else if(dist>40 && dist<=50){
     digitalWrite(LED5,HIGH);
     digitalWrite(LED4,LOW);
     digitalWrite(LED3,LOW);
     digitalWrite(LED2,LOW);
-    digitalWrite(LED1,LOW);  
+    digitalWrite(LED1,LOW);
+    Blynk.virtualWrite(V1, 50-dist);  
   }  
   else if(dist>30 && dist<=40){
     digitalWrite(LED5,HIGH);
     digitalWrite(LED4,HIGH);
     digitalWrite(LED3,LOW);
     digitalWrite(LED2,LOW);
-    digitalWrite(LED1,LOW);  
+    digitalWrite(LED1,LOW);
+    Blynk.virtualWrite(V1, 50-dist);  
   }  
   else if(dist>20 && dist<=30){
     digitalWrite(LED5,HIGH);
@@ -87,6 +117,7 @@ void showStatus(int dist){
     digitalWrite(LED3,HIGH);
     digitalWrite(LED2,LOW);
     digitalWrite(LED1,LOW);  
+    Blynk.virtualWrite(V1, 50-dist);
   }
   else if(dist>10 && dist<=20){
     digitalWrite(LED5,HIGH);
@@ -94,6 +125,7 @@ void showStatus(int dist){
     digitalWrite(LED3,HIGH);
     digitalWrite(LED2,HIGH);
     digitalWrite(LED1,LOW);  
+    Blynk.virtualWrite(V1, 50-dist);
   }
   
   else if(dist>0 && dist<=10){
@@ -102,6 +134,7 @@ void showStatus(int dist){
     digitalWrite(LED3,HIGH);
     digitalWrite(LED2,HIGH);
     digitalWrite(LED1,HIGH);
+    Blynk.virtualWrite(V1, 50-dist);
     if(DEBUG){
       Serial.println("Dustbin is full,no more space.");
     }  
